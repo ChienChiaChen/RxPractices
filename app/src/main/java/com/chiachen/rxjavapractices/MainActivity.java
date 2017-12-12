dad861244b4bc59dc2ff2a9a16c60b2df1c5cfc1
JasonChien <s124202468@gmail.com>
2017-12-12 16:29:50 +0800

    rx example 1

--- app/src/main/java/com/chiachen/rxjavapractices/MainActivity.java
+++ app/src/main/java/com/chiachen/rxjavapractices/MainActivity.java
@@ -1,13 +1,56 @@
 package com.chiachen.rxjavapractices;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.functions.Consumer;
+import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = "Jason_Chien";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Observable<Integer> observable = Observable.create(new ObservableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(ObservableEmitter<Integer> emitter) throws Exception {
+                Log.d(TAG, "Observable thread is : " + Thread.currentThread().getName());
+                Log.d(TAG, "emit 1");
+                emitter.onNext(1);
+            }
+        });
+
+        Consumer<Integer> consumer = new Consumer<Integer>() {
+            @Override
+            public void accept(Integer integer) throws Exception {
+                Log.d(TAG, "Observer thread is :" + Thread.currentThread().getName());
+                Log.d(TAG, "onNext: " + integer);
+            }
+        };
+
+        observable.subscribeOn(Schedulers.newThread())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .doOnNext(new Consumer<Integer>() {
+                        @Override
+                        public void accept(Integer integer) throws Exception {
+                            Log.d(TAG, "After observeOn(mainThread), current thread is: " + Thread.currentThread().getName());
+                        }
+                    })
+                    .observeOn(Schedulers.io())
+                    .doOnNext(new Consumer<Integer>() {
+                        @Override
+                        public void accept(Integer integer) throws Exception {
+                            Log.d(TAG, "After observeOn(io), current thread is : " + Thread.currentThread().getName());
+                        }
+                    })
+                    .subscribe(consumer);
     }
 }
