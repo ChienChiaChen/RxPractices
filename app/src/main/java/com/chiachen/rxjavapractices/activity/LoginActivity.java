e28e8dd25d6428ccdd938e2c07f4ab01b242a41e
JasonChien <s124202468@gmail.com>
2017-12-19 18:24:44 +0800

    Add listener

--- app/src/main/java/com/chiachen/rxjavapractices/activity/LoginActivity.java
+++ app/src/main/java/com/chiachen/rxjavapractices/activity/LoginActivity.java
@@ -3,6 +3,8 @@ package com.chiachen.rxjavapractices.activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import com.chiachen.rxjavapractices.R;
 
@@ -11,10 +13,27 @@ import com.chiachen.rxjavapractices.R;
  */
 
 public class LoginActivity extends AppCompatActivity {
+    private String account, pwd;
+    private Button register, login;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        findViewById(R.id.login).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 1-1 Get data
+                // 1-2 Data
+                // 1-3
+            }
+        });
+        findViewById(R.id.pwd).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Post data
+            }
+        });
     }
 }
