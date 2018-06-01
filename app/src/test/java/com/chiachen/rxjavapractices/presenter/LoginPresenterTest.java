98bc5e32dca4ac1d450777efa12656f70a592cbb
JasonChien <s124202468@gmail.com>
2018-06-01 18:07:38 +0800

    Add login test case.

--- app/src/test/java/com/chiachen/rxjavapractices/presenter/LoginPresenterTest.java
+++ app/src/test/java/com/chiachen/rxjavapractices/presenter/LoginPresenterTest.java
@@ -8,17 +8,26 @@ import com.chiachen.rxjavapractices.view.LoginView;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Matchers;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
 import java.util.List;
 
 import io.reactivex.Observable;
+import io.reactivex.Observer;
 
 /**
  * Created by jianjiacheng on 2018/05/21.
  */
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(Observable.class)
 public class LoginPresenterTest {
 
     @Mock
@@ -32,30 +41,52 @@ public class LoginPresenterTest {
 
     private LoginPresenter mLoginPresenter;
 
+    private SchedulerProvider mSchedulerProvider;
+
     @Before
     public void setUp() throws Exception {
         MockitoAnnotations.initMocks(this);
-        SchedulerProvider mSchedulerProvider = new TrampolineSchedulerProvider();
+        mSchedulerProvider = new TrampolineSchedulerProvider();
         mLoginPresenter = new LoginPresenter(mLoginView, mApi, mSchedulerProvider);
     }
 
     @Test
     public void triggerLogin_success() throws Exception {
-        Mockito
-                .doReturn(Observable.just(mLoginResponses))
-                .when(mApi).
-                login("My","Soul");
-
-        mLoginPresenter.triggerLogin("My","Soul");
+        Mockito.doReturn(Observable.just(mLoginResponses)).when(mApi).login("My", "Soul");
+        mLoginPresenter.triggerLogin("My", "Soul");
         Mockito.verify(mLoginView).showToast("Login Success");
         Mockito.verify(mLoginView).openHomeActivity();
     }
 
+    //Ref: https://github.com/anupcowkur/MVPSample.
+    @Test
+    public void triggerLogin_success_1() throws Exception {
+        // Observable productsObservable = PowerMockito.mock(Observable.class);
+
+
+        //create mocks
+        Observable<List<LoginResponse>> observable = (Observable<List<LoginResponse>>) PowerMockito.mock(Observable.class);
+
+        Mockito.when(mApi.login("Test","Test")).thenReturn(observable);
+        Mockito.when(observable.subscribeOn(mSchedulerProvider.io())).thenReturn(observable);
+        Mockito.when(observable.observeOn(mSchedulerProvider.ui())).thenReturn(observable);
+
+        // define return values
+        mLoginPresenter.triggerLogin("Test","Test");
+
+        Mockito.verify(mApi).login("Test","Test");
+        Mockito.verify(observable).subscribeOn(mSchedulerProvider.io());
+        Mockito.verify(observable).observeOn(mSchedulerProvider.ui());
+        Mockito.verify(observable).subscribe(Matchers.<Observer<List<LoginResponse>>>any());
+        // Matchers.<Consumer<List<LoginResponse>>>any();
+        // Mockito.verify(observable).subscribe();
+    }
+
     @Test
     public void triggerLogin_fail() throws Exception {
-        Mockito.doReturn(Observable.error(new Exception())).when(mApi).login("ff","ff");
+        Mockito.doReturn(Observable.error(new Exception())).when(mApi).login("ff", "ff");
 
-        mLoginPresenter.triggerLogin("ff","ff");
+        mLoginPresenter.triggerLogin("ff", "ff");
         Mockito.verify(mLoginView).isErrorInput();
     }
 
